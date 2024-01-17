e1bcbe2d41bde706671dee09aaf50c6fee7751c7
viniciochavess <viniciochavess@gmail.com>
2024-01-16 23:03:26 -0300

    Operadores lógico

---
+++ basic python/LogicalOperators.py
@@ -0,0 +1,22 @@
+print(True and True) # True
+print(True and False) # False
+print(True or True) # True
+print(True or False) #True
+print(False or False) # False
+print(not(True)) # False
+print(True and not(False))#True
+print(not(False) and not(False))#True
+print(5==5)#True
+print(5!=5)#False
+
+if(True):
+    print("Verdade")
+else:
+    print("falso")
+
+if(False):
+    print("Verdade")
+elif(True):
+    print("Elif")
+else:
+    print('default')
